6ab261d0002261a5fdcdb69bf18511f6aaf90622
Esteban Sánchez <e.sanchez@globant.com>
2020-12-03 15:19:16 -0600

    debugger

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const playerDOM = document.getElementById('player');
   var player;
   let castReady = false;
+  const debuggerDOM = document.getElementById('debugger');
   
   function messageListener(e) {
     const {type, data} = e;
   
+    debuggerDOM.innerHTML = JSON.stringify(data, null, 2);
+
     switch (data.command) {
       case 'INIT_COMMUNICATION':
         playerDOM.setAttribute('data-plyr-provider', data.provider);
